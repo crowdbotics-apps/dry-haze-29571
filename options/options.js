c424cf914cc4e71b8b1611af539e4933627cad99
Crowdbotics <team@crowdbotics.com>
2021-08-06 16:49:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dry_haze_29571Identifier",
+  url: "https://dry_haze_29571Identifier.botics.co",
+  api: "https://dry_haze_29571Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
